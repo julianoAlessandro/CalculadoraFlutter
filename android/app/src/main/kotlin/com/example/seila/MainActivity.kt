33f1353c0225bd9b2fa86ab20c188b95b194a7d8
Juliano <julianoalessandro08@gmail.com>
2024-06-07 10:23:31 -0300

    Calculadora finalizada

---
+++ android/app/src/main/kotlin/com/example/seila/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.seila
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/Calculadora.dart
@@ -0,0 +1,148 @@
+import 'package:flutter/material.dart';
+
+import 'Memoria.dart'; // Corrigindo o nome do arquivo e importação
+
+class Calculadora extends StatefulWidget {
+  @override
+  _CalculadoraState createState() => _CalculadoraState();
+}
+
+class _CalculadoraState extends State<Calculadora> {
+  final _memoria = Memoria(); // Corrigindo o nome da classe para Memoria
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.grey,
+        title: Text('CALCULADORA ATIVIDADE DOUGLAS'),
+      ),
+      body: Column(
+        children: <Widget>[
+          _buildDisplay(),
+          Divider(height: 0.1),
+          _buildKeyboard(),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildDisplay() {
+    return Expanded(
+      flex: 1,
+      child: Container(
+        color: Colors.black,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                _memoria.result,
+                textAlign: TextAlign.end,
+                style: TextStyle(
+                  fontWeight: FontWeight.w200,
+                  decoration: TextDecoration.none,
+                  fontSize: 80.0,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildKeyboardButton(String label,
+      {int flex = 1, Color textColor = Colors.green, Color backgroundColor = Colors.white}) {
+    return Expanded(
+      flex: flex,
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+
+        ),
+        child: Text(
+          label,
+          style: TextStyle(fontSize: 24, color: textColor),
+        ),
+        onPressed: () {
+          setState(() {
+            _memoria.applyCommand(label);
+          });
+        },
+      ),
+    );
+  }
+
+  Widget _buildKeyboard() {
+    return Container(
+      color: Colors.black,
+      height: 400.0,
+      child: Column(
+        children: <Widget>[
+          Expanded(
+            flex: 1,
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                _buildKeyboardButton('AC', textColor: Colors.deepOrange),
+                _buildKeyboardButton('DEL', textColor: Colors.deepOrange),
+                _buildKeyboardButton('%', textColor: Colors.deepOrange),
+                _buildKeyboardButton('/', textColor: Colors.white),
+              ],
+            ),
+          ),
+          Expanded(
+            flex: 1,
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                _buildKeyboardButton('7'),
+                _buildKeyboardButton('8'),
+                _buildKeyboardButton('9'),
+                _buildKeyboardButton('*', textColor: Colors.deepOrange),
+              ],
+            ),
+          ),
+          Expanded(
+            flex: 1,
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                _buildKeyboardButton('4'),
+                _buildKeyboardButton('5'),
+                _buildKeyboardButton('6'),
+                _buildKeyboardButton('+', textColor: Colors.deepOrange),
+              ],
+            ),
+          ),
+          Expanded(
+            flex: 1,
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                _buildKeyboardButton('1'),
+                _buildKeyboardButton('2'),
+                _buildKeyboardButton('3'),
+                _buildKeyboardButton('-', textColor: Colors.deepOrange),
+              ],
+            ),
+          ),
+          Expanded(
+            flex: 1,
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                _buildKeyboardButton('0', flex: 2),
+                _buildKeyboardButton('.'),
+                _buildKeyboardButton('=', textColor: Colors.deepOrange),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
